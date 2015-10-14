4178550e5e3b816f1346b6abfdd50fb28be94d7c
arttu <d15123109@mydit.ie>
2015-10-14 19:35:20 +0300

    Lab 5 stuff

---
+++ lab5/my_application/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Index File" 
+
+@app.route("/hello")
+def hello():
+    return "Hello World"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return "User %s" % username
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return "Post %d" % post_id
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
